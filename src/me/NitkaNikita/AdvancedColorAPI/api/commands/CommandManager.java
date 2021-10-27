95601da7104a2c37703fa6c8584df9f46607489d
nitkanikita21 <waleryvvs1@gmail.com>
2021-10-27 17:28:51 +0300

    added .iml

--- src/me/NitkaNikita/AdvancedColorAPI/api/commands/CommandManager.java
+++ src/me/NitkaNikita/AdvancedColorAPI/api/commands/CommandManager.java
@@ -1,5 +1,8 @@
 package me.NitkaNikita.AdvancedColorAPI.api.commands;
 
+import me.NitkaNikita.AdvancedColorAPI.api.types.AdvancedColor;
+import me.NitkaNikita.AdvancedColorAPI.api.types.BaseChatComponent;
+import me.NitkaNikita.AdvancedColorAPI.api.types.Components.SolidColor;
 import me.NitkaNikita.AdvancedColorAPI.api.utils.RegExpUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,8 +21,15 @@ public class CommandManager implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
 
-        if(args.length == 0){
+        if(args.length <= 0){
             commandSender.sendMessage(ChatColor.AQUA+"[AdvacedColorAPI]"+ChatColor.GRAY+" version: "+ChatColor.LIGHT_PURPLE+"1.4");
+        }else{
+            AdvancedColor color = new AdvancedColor(args[0]);
+            BaseChatComponent solid1 = new SolidColor(args[1],color);
+            BaseChatComponent solid2 = new SolidColor(args[2],color);
+
+            solid1.addComponent(solid2);
+            commandSender.sendMessage(solid1.renderComponent().toLegacyText());
         }
 
         return true;
